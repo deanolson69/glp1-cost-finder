f5c6be2fcbcdc29071caf24ef0ebeea0b9743ada
deanolson69 <129903808+deanolson69@users.noreply.github.com>
2026-04-17 15:33:34 -0400

    Initialize main.jsx with ReactDOM rendering

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import GLP1CostFinder from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GLP1CostFinder />
+  </React.StrictMode>,
+)
